08c71e4117ff7b4f46ea5a2e0459ffdb8ffe2a8d
madao <madao@mydebian.zte.com.cn>
2018-11-30 02:58:51 +0200

    added 0x prefix to key

--- scripts/install_ansible.sh
+++ scripts/install_ansible.sh
@@ -7,16 +7,16 @@ set -e
 echo "deb http://ppa.launchpad.net/ansible/ansible/ubuntu trusty main" | sudo tee -a /etc/apt/sources.list
 
 #Add ansible public gpg key to apt
-sudo apt-key adv --keyserver keyserver.ubuntu.com --recv-keys 93C4A3FD7BB9C367
+sudo apt-key adv --keyserver keyserver.ubuntu.com --recv-keys 0x93C4A3FD7BB9C367
 
 #Update apt
 sudo apt-get update
 
 #Install Ansible
-sudo apt-get install ansible
+sudo apt-get install -y ansible
 
 #Install dependency library to work with aws
 pip install boto 
 
 #Export ANSIBLE-CONFIG , this allows ansible to use the ansible.cfg from a world writable directory
-export ANSIBLE_CONFIG=~/iac-tutorial/ansible/ansible.cfg
+export ANSIBLE_CONFIG=~/el-project/ansible/ansible.cfg
